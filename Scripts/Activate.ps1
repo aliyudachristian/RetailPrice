3bb85f24563e7a5207b86aad6247c1000a8b774e
aliyudachristian <aliyudachristian24@gmail.com>
2023-03-18 10:30:00 +0500

    Add unit tests

--- Scripts/Activate.ps1
+++ Scripts/Activate.ps1
@@ -174,9 +174,7 @@ Write-Verbose "Activation script is located in path: '$VenvExecPath'"
 Write-Verbose "VenvExecDir Fullname: '$($VenvExecDir.FullName)"
 Write-Verbose "VenvExecDir Name: '$($VenvExecDir.Name)"
 
-# Set values required in priority: CmdLine, ConfigFile, Default
-# First, get the location of the virtual environment, it might not be
-# VenvExecDir if specified on the command line.
+
 if ($VenvDir) {
     Write-Verbose "VenvDir given as parameter, using '$VenvDir' to determine values"
 }
