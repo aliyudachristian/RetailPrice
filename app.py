ac549e2333200613b5dad80b42eacbcbdb54b007
aliyudachristian <aliyudachristian24@gmail.com>
2023-04-07 16:05:25 +0500

    adding new feature

--- app.py
+++ app.py
@@ -57,3 +57,28 @@ elif selected_chart == 'Bar Chart - Price Difference':
     avg_price_diff_by_category = data.groupby('product_category_name')['comp_price_diff'].mean().reset_index()
     fig = px.bar(avg_price_diff_by_category, x='product_category_name', y='comp_price_diff')
     st.plotly_chart(fig)
+
+# Train a Decision Tree Regressor 
+st.sidebar.header('Modeling Options')
+model_button = st.sidebar.button('Train Decision Tree Regressor Model')
+
+if model_button:
+    st.header('Predicted vs Actual Retail Price')
+    data['comp_price_diff'] = data['unit_price'] - data['comp_1']
+    
+    X = data[['qty', 'unit_price', 'comp_1', 'product_score', 'comp_price_diff']]
+    y = data['total_price']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    model = DecisionTreeRegressor()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=y_test, y=y_pred, mode='markers', marker=dict(color='blue'), name='Predicted vs. Actual'))
+    fig.add_trace(go.Scatter(x=[min(y_test), max(y_test)], y=[min(y_test), max(y_test)], mode='lines', marker=dict(color='red'), name='Ideal Prediction'))
+    fig.update_layout(title='Predicted vs Actual Retail Price', xaxis_title='Actual Retail Price', yaxis_title='Predicted Retail Price')
+    st.plotly_chart(fig)
+    
+    mse = mean_squared_error(y_test, y_pred)
+    st.write(f'Mean Squared Error: {mse:.2f}')
