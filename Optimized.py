5e1257882e7921c1304df861d76bd8d29e459386
aliyudachristian <aliyudachristian24@gmail.com>
2023-06-30 08:59:10 +0500

    adding Visual

--- Optimized.py
+++ Optimized.py
@@ -27,3 +27,50 @@ chart_options = {
     'Bar Chart - Price Difference': 'Average Competitor Price Difference by Product Category'
 }
 selected_chart = st.sidebar.selectbox('Select a visualization:', list(chart_options.keys()))
+
+# Visualization
+st.header(chart_options[selected_chart])
+if selected_chart == 'Histogram':
+    fig = px.histogram(data, x='total_price', nbins=20)
+elif selected_chart == 'Box Plot':
+    fig = px.box(data, y='unit_price')
+elif selected_chart == 'Scatter Plot':
+    fig = px.scatter(data, x='qty', y='total_price', trendline='ols')
+elif selected_chart == 'Bar Chart':
+    avg_price_by_category = data.groupby('product_category_name')['total_price'].mean().reset_index()
+    fig = px.bar(avg_price_by_category, x='product_category_name', y='total_price')
+elif selected_chart == 'Correlation Heatmap':
+    numeric_columns = data.select_dtypes(include=['float64', 'int64']).columns
+    correlation_matrix = data[numeric_columns].corr()
+    fig = go.Figure(go.Heatmap(x=correlation_matrix.columns, y=correlation_matrix.columns, z=correlation_matrix.values))
+elif selected_chart == 'Bar Chart - Price Difference':
+    data['comp_price_diff'] = data['unit_price'] - data['comp_1']
+    avg_price_diff_by_category = data.groupby('product_category_name')['comp_price_diff'].mean().reset_index()
+    fig = px.bar(avg_price_diff_by_category, x='product_category_name', y='comp_price_diff')
+st.plotly_chart(fig)
+
+# Sidebar - Modeling Options
+st.sidebar.header('Modeling Options')
+model_button = st.sidebar.button('Train Decision Tree Regressor Model')
+
+# Model Training and Display Predictions
+if model_button:
+    st.header('Predicted vs Actual Retail Price')
+    data['comp_price_diff'] = data['unit_price'] - data['comp_1']
+    
+    X = data[['qty', 'unit_price', 'comp_1', 'product_score', 'comp_price_diff']]
+    y = data['total_price']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    model = DecisionTreeRegressor()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=y_test, y=y_pred, mode='markers', marker=dict(color='blue'), name='Predicted vs. Actual'))
+    fig.add_trace(go.Scatter(x=[min(y_test), max(y_test)], y=[min(y_test), max(y_test)], mode='lines', marker=dict(color='red'), name='Ideal Prediction'))
+    fig.update_layout(title='Predicted vs Actual Retail Price', xaxis_title='Actual Retail Price', yaxis_title='Predicted Retail Price')
+    st.plotly_chart(fig)
+    
+    mse = mean_squared_error(y_test, y_pred)
+    st.write(f'Mean Squared Error: {mse:.2f}')
